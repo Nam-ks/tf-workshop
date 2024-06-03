454b14e0de86503ee51c4a6ac485d69af548123c
root <admin@example.com>
2024-06-03 16:23:45 +0900

    no bastion

--- EKS_cluster/main.tf
+++ EKS_cluster/main.tf
@@ -97,7 +97,7 @@ module "eks" {
   create_iam_role = false
   # 기존에 있던 role matching 
   iam_role_arn = "arn:aws:iam::552166050235:role/eksClusterRole"
-  
+  kms_key_aliases = "eks1/nam-terra-eks"
   tags = {
     Name = "${local.tag}_eks_cluster"
   }
@@ -159,7 +159,7 @@ resource "aws_instance" "BastionHost" {
   ami                         = "ami-0b9a26d37416470d2"
   instance_type               = local.bastion_instance_type
   key_name                    = data.aws_key_pair.EC2-Key.key_name
-  subnet_id                   = local.public_subnets[0]
+  subnet_id                   = local.public_subnets[0].id
   associate_public_ip_address = true
   vpc_security_group_ids      = [module.BastionHost_SG.security_group_id, module.eks.cluster_security_group_id]
 
