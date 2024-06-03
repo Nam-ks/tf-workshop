75d6ee475446ae2c3f8527890ce034b633999f9f
root <admin@example.com>
2024-06-03 15:04:31 +0900

    bastion ami

--- EKS_cluster/main.tf
+++ EKS_cluster/main.tf
@@ -155,7 +155,7 @@ data "aws_key_pair" "EC2-Key" {
 # BastionHost Instance
 # EKS Cluster SG : data.aws_eks_cluster.cluster.vpc_config[0].cluster_security_group_id 
 resource "aws_instance" "BastionHost" {
-  ami                         = "ami-0ea4d4b8dc1e46212"
+  ami                         = "ami-0b9a26d37416470d2"
   instance_type               = local.bastion_instance_type
   key_name                    = data.aws_key_pair.EC2-Key.key_name
   subnet_id                   = local.public_subnets[0]
