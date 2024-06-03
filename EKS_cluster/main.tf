a62acd0fe3fd8a595b528d45e726cb30e311a6cd
root <admin@example.com>
2024-06-03 16:38:29 +0900

    k8s provider

--- EKS_cluster/main.tf
+++ EKS_cluster/main.tf
@@ -62,6 +62,22 @@ module "vpc" {
 # 워커 노드 그룹의 최소 최대 요구 개수를 입력받아 변경 가능하고 기본값이 각 232로 지정
 # role은 미리 정의하여 배포되어 있다고 가정
 #----------------------------------------------------------------------------#
+# AWS EKS Cluster Data Source
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+# AWS EKS Cluster Auth Data Source
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.12.0"
