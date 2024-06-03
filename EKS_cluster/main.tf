033f6061e7b3f1bdb88b612ab5a279166f0731cf
root <admin@example.com>
2024-06-03 18:26:31 +0900

    k8s

--- EKS_cluster/main.tf
+++ EKS_cluster/main.tf
@@ -102,14 +102,14 @@ module "eks" {
   create_iam_role = false
   # 기존에 있던 role matching 
   iam_role_arn              = "arn:aws:iam::552166050235:role/eksClusterRole"
-  manage_aws_auth_configmap = true
-  aws_auth_users = [
-    {
-      userarn  = "arn:aws:iam::552166050235:user/admin"
-      username = "admin"
-      groups   = ["system:masters"]
-    },
-  ]
+  # manage_aws_auth_configmap = true
+  # aws_auth_users = [
+  #   {
+  #     userarn  = "arn:aws:iam::552166050235:user/admin"
+  #     username = "admin"
+  #     groups   = ["system:masters"]
+  #   },
+  # ]
 
   tags = {
     Name = "${local.tag}_eks_cluster"
