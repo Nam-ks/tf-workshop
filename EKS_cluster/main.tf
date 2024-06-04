ade43574ed5bd6b21c5ecafc62e333811cdcd3b8
root <admin@example.com>
2024-06-04 15:13:22 +0900

    not default SG

--- EKS_cluster/main.tf
+++ EKS_cluster/main.tf
@@ -110,6 +110,7 @@ module "eks" {
   cluster_endpoint_public_access  = true
   vpc_id                          = module.vpc.vpc_id
   subnet_ids                      = module.vpc.private_subnets
+  create_cluster_security_group = false
   cluster_security_group_id = module.eks_SG.security_group_id
   create_kms_key = false
   cluster_encryption_config = {
