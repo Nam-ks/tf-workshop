cd3b67e0fdceb4037cc284142bd69065cd92d87b
root <admin@example.com>
2024-06-05 14:10:16 +0900

    alb_controller_5

--- K8s_resource/variables.tf
+++ K8s_resource/variables.tf
@@ -1,8 +1,7 @@
 locals {
   region                    = "ap-northeast-1"
-  oidc_provider_arn = var.eks_oidc
+  oidc_provider_arn = var.oidc_provider_arn
   tag               = "nam-terra"
-  account_id                = var.account_id
 }
 
 variable "account_id" {
@@ -15,4 +14,10 @@ variable "eks_oidc" {
   description = "eks oidc"
   type        = string
   default     = "E6569989B19EE804C1D6C4CE94EE14EC"
+}
+
+variable "oidc_provider_arn" {
+  description = "cluster oidc arn"
+  type        = string
+  default     = "https://E6569989B19EE804C1D6C4CE94EE14EC.gr7.ap-northeast-1.eks.amazonaws.com"
 }
