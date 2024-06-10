68a51039ec47986662c1d719c0a7f45ff3108db8
root <admin@example.com>
2024-06-10 09:58:37 +0900

    k8s_alb_con

--- K8s_resource/main.tf
+++ K8s_resource/main.tf
@@ -96,3 +96,26 @@ resource "kubernetes_service_account" "aws-load-balancer-controller" {
 
   depends_on = [module.load_balancer_controller_irsa_role]
 }
+
+
+resource "helm_release" "aws-load-balancer-controller" {
+  name       = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+
+  set {
+    name  = "clusterName"
+    value = local.tag
+  }
+  set {
+    name  = "serviceAccount.create"
+    value = false
+  }
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  depends_on = [kubernetes_service_account.aws-load-balancer-controller]
+}
