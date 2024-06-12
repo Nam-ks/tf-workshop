0937564a888c588c7cdd20c9d5035b04b9d93db2
root <admin@example.com>
2024-06-12 14:17:21 +0900

    env test

--- K8s_resource/main.tf
+++ K8s_resource/main.tf
@@ -123,7 +123,7 @@ resource "kubernetes_namespace" "cloudnetworks" {
 }
 resource "kubernetes_deployment" "namserver" {
   metadata {
-    name = "namserver"
+    name = "${test}-namserver"
     namespace = "cloudnetworks"
     labels = {
       test = "namserver"
